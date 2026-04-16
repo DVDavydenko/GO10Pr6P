908f51f84b26ff97293f5dacf25266dddd49166b
DVDavydenko <dvdaydenko@gmail.com>
2026-04-17 01:12:46 +0300

    Update api.js

--- api.js
+++ api.js
@@ -38,6 +38,7 @@ async function sendToGoogleSheets(payload) {
     ...payload
   });
 }
+
 async function getStudentCode(studentName, classValue) {
   return await postToGAS({
     action: 'getStudentCode',
